9688adbdd8b348a40ad2b52499a88ffd7bf448b3
Magno Sescon <magnosescon@gmail.com>
2021-02-11 20:26:46 -0300

    chore: refactoring the index of componentes

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -1,4 +1,1 @@
-export { default as Form } from './form';
-export { default as Title } from './title'
-export { default as List } from './list/index';
-export {default as Todo } from './todo/index';
+export { default as Routes } from './routes/routes';
