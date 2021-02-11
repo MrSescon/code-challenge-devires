380db789383ffceecb7b3483b5d1ac6e8c042c21
Magno Sescon <magnosescon@gmail.com>
2021-02-11 17:56:17 -0300

    refactor: created a todolist's component index and refactored the TodoList component

--- src/presentation/pages/todoList/index.tsx
+++ src/presentation/pages/todoList/index.tsx
@@ -1,94 +1,23 @@
-import React, { useEffect } from 'react'
-import {Form, Title, List } from '../../components'
+import React, {useState, useEffect} from 'react';
+import { Form, Title, List } from './components';
+import { Container, ListContainer } from './styles';
 
 const TodoList: React.FC = () => {
+  const [ todos, setTodos ] = useState([]);
 
-	const introData = [
-		{
-			id: -3, 
-			value: "Hi! This is a simple todo list app made by REACT <3"
-		},
-		{
-			id: -2,
-			value: "Hover over todos and click on `X` to delete them!"
-		},
-		{
-			id: -1,
-			value: "Add new todos and come back any time later, I will save them for you!"
-		}
-	];
-	
-	// Handler to add todo
-	const addTodo = (val: string) => {
-		// let id;
-		// // if localStorage is available then increase localStorage count
-		// // else use global window object's id variable
-		// if (typeof(Storage) !== "undefined") {
-		// 	id = Number(localStorage.count);
-		// 	localStorage.count = Number(localStorage.count) + 1;
-		// } else {
-		// 	id = window.id++;
-		// }
-		
-		// const todo = { 
-		// 	value: val, 
-		// 	id: id 
-		// };
-		
-		// this.state.data.push(todo);
-		// // update state
-		// this.setState({
-		// 	data: this.state.data
-		// }, () => {
-		// 	// update localStorage
-		// 	this.updateLocalStorage();
-		// });
-	}
-	// Handler to remove todo
-	const removeTodo = (id: number) => {
-		// // filter out the todo that has to be removed
-		// const list = this.state.data.filter(todo => {
-		// 	if (todo.id !== id)
-		// 		return todo;
-		// });
-		// // update state
-		// this.setState({
-		// 	data: list
-		// }, () => {
-		// 	// update localStorage
-		// 	this.updateLocalStorage();
-		// });
-	}
-	
-	useEffect(() => {
-		// localStorage.clear();
-		// if (typeof(Storage) !== "undefined") {
-		// 	if(!localStorage.todos) {
-		// 		localStorage.todos = JSON.stringify(this.state.data);
-		// 	}
-		// 	if(!localStorage.count) {
-		// 		localStorage.count = 0;
-		// 	}
+  useEffect(() => {
 
-		// } else {
-		// 	 console.log("%cApp will not remember todos created as LocalStorage Is Not Available",
-		// 					 "color: hotpink; background: #333; font-size: x-large;font-family: Courier;");
-		// 	window.id = 0;
-		// }
-	}, [])
-	
-	return (
-		<div id="container">
-			<Title />
-			<Form 
-				// addTodo={addTodo} 
-			/>
-			<List
-				// todos={state.data} 
-				// remove={removeTodo} 
-			/>
-		</div>
-	);
-}
+  },[]);
+
+  return (
+    <Container>
+      <ListContainer>
+        <Title />
+        <Form />
+        <List />
+      </ListContainer>
+    </Container>
+  );
+};
 
 export default TodoList;
--- src/presentation/pages/todoList/styles.ts
+++ src/presentation/pages/todoList/styles.ts
@@ -1,53 +1,67 @@
 import styled from 'styled-components';
 import Colors from '../../styles/colors';
 
-// *
-// 	font-family: Bungee Hairline !important
-// 	font-weight: bold
-// 	color: $black
-// 	border-color: $black 
-// 	line-height: 1.5
-
-
-// \::-webkit-input-placeholder
-// 	color: rgba($pink, .5)
-
-// \:-moz-placeholder
-// 	color: rgba($pink, .5)
-
-// \::-moz-placeholder
-// 	color: rgba($pink, .5)
-
-// \:-ms-input-placeholder
-// 	color: rgba($pink, .5)
-
-// body
-// 	background: #fefefe
-
-
-export const container = styled.div`
-	margin: auto;
-	text-align: center;
-	max-width: 700px;
-	margin: 60px auto 0 auto;
-	padding: 0px 20px;
-	
-	h2 {
-		margin-bottom: 30px;
-		color: ${Colors.black};
-  }
-	
-	#info {
-		padding: 7px;
-		text-align: left;
-		color: ${Colors.grey};
-  }
-
-	#acu {
-		text-align: left;
-		padding: 7px;
-  }
+export const Container = styled.div`
+  font-family: Bungee Hairline !important;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  color: ${Colors.black};
+  background: ${Colors.primary};
 `;
 
-			
-			
+export const ListContainer = styled.div`
+  align-items: center;
+  background: ${Colors.primaryDark};
+  font-weight: bold;
+  line-height: 1.5;
+  margin: auto;
+  text-align: center;
+  width: 80%;
+  height: 80%;
+  margin: 60px auto 0 auto;
+  padding: 0px 20px;
+  border-radius: 5px;
+  border: 1px solid rgba(255, 255, 255, 0.1);
+  box-shadow: 0px 5px 12px rgba(0, 0, 0, 0.1);
+
+  #info {
+    font-size: 1rem;
+    padding: 7px;
+    text-align: left;
+    color: ${Colors.textInPrimary};
+  }
+
+  #acu {
+    font-size: 1.5rem;
+    text-align: left;
+    padding: 7px;
+  }
+
+  @media (min-width: 1100px) {
+    #info {
+      font-size: 4rem;
+    }
+
+    #acu {
+      font-size: 6rem;
+    }
+  }
+
+  \::-webkit-input-placeholder {
+    color: ${Colors.pink};
+  }
+
+  \:-moz-placeholder {
+    color: ${Colors.pink};
+  }
+
+  \::-moz-placeholder {
+    color: ${Colors.pink};
+  }
+
+  \:-ms-input-placeholder {
+    color: ${Colors.pink};
+  }
+`;
