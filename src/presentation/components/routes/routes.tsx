d4363ae1b8d24e3d5307cc0cd7221a7eb83540db
Magno Sescon <magnosescon@gmail.com>
2021-02-10 21:54:43 -0300

    feat: added the todoList route

--- src/presentation/components/routes/routes.tsx
+++ src/presentation/components/routes/routes.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import Landing from '../../pages/Landing';
+import Landing from '../../pages/landing';
+import TodoList from '../../pages/todoList'
 
 const routes: React.FC = () => {
   return (
     <BrowserRouter>
       <Route path="/" exact component={Landing} />
+      <Route path="/todoList" exact component={TodoList} />
     </BrowserRouter>
   );
 };
